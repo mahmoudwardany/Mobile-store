7233fc4ca805b5eaafe513897267d7e2faef1a82
Mahmoud Wardany <106314476+mahmoudwardany@users.noreply.github.com>
2022-07-16 17:26:22 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,17 +3,7 @@ let pro =document.querySelector('.product')
 
 function toggleclass(){
 pro.classList.toggle('show')
+  btn.innerHTML='hide'
 
 }
 btn.addEventListener('click', toggleclass)
-
-
-// myTxT.className = 'txt';
-//   myTxT.classList.add('txt')
-//   myTxT.classList.remove('test')
-// myTxT.classList.toggle('txt')
-// }
-
-// let myTxT = document.querySelector('.test');
-// let btn = document.querySelector('button');
-// function toggleClass(){
