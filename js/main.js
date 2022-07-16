35eff1bdd7ae6141e8eed12a6ff47251c0fe9214
Mahmoud Wardany <106314476+mahmoudwardany@users.noreply.github.com>
2022-07-16 18:27:40 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ let btn =document.getElementById('btn')
 let pro =document.querySelector('.product')
 
 function toggleclass(){
+if(btn.innerHTML=='Show More'){
 pro.classList.toggle('show')
-  btn.innerHTML='hide'
+btn.innerHTML='Hide'
+}else{
+    btn.innerHTML='Show More'
+}
 
 }
 btn.addEventListener('click', toggleclass)
