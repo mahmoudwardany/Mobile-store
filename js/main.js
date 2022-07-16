2b2074a1bfd6381b86125657e69ff73456d363ab
Mahmoud Wardany <106314476+mahmoudwardany@users.noreply.github.com>
2022-07-16 17:21:51 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let btn =document.getElementById('btn')
+let pro =document.querySelector('.product')
+
+function toggleclass(){
+pro.classList.toggle('show')
+
+}
+btn.addEventListener('click', toggleclass)
+
+
+// myTxT.className = 'txt';
+//   myTxT.classList.add('txt')
+//   myTxT.classList.remove('test')
+// myTxT.classList.toggle('txt')
+// }
+
+// let myTxT = document.querySelector('.test');
+// let btn = document.querySelector('button');
+// function toggleClass(){
